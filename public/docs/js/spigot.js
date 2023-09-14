213559257845f5d39ab68d34e46f1f2638a1912f
srnyx <25808801+srnyx@users.noreply.github.com>
2023-09-13 22:16:01 -0400

    Add `LATEST` Spigot Javadocs button

--- public/docs/js/spigot.js
+++ public/docs/js/spigot.js
@@ -7,7 +7,7 @@ if (path) {
     if (displayPath.endsWith('.html')) {
         displayPath = displayPath.slice(0, -5);
     } else if (displayPath.includes('.html#')) {
-        displayPath = displayPath.replace('.html#', '#')
+        displayPath = displayPath.replace('.html#', '#');
     }
     document.getElementById('header').textContent = displayPath.replaceAll('/', '.');
 }
@@ -16,7 +16,7 @@ for (const button of buttons) {
     button.addEventListener('click', () => {
         const version = button.textContent;
         const compiledPath = getPath(version);
-        let url = `https://helpch.at/docs/${version}`;
+        let url = version === 'LATEST' ? 'https://hub.spigotmc.org/javadocs/spigot' : `https://helpch.at/docs/${version}`;
         if (compiledPath) url += `/${compiledPath}`;
         window.location.href = url;
     });
@@ -26,7 +26,11 @@ const dashes = ['1.15.2', '1.15.1', '1.15', '1.14.4', '1.14.3', '1.14.2', '1.14.
 const percentTwenty = ['1.11.2', '1.11.1', '1.11', '1.10.2', '1.10', '1.9.4', '1.9.2', '1.9.1', '1.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8', '1.7.10', '1.7.9', '1.7.8', '1.7.5', '1.7.2', '1.6.2', '1.5']
 
 function getPath(version) {
-    if (dashes.includes(version)) return path.replace(/[,()]/g, '-')
-    if (percentTwenty.includes(version)) return path.replace(',', ',%20')
+    if (dashes.includes(version)) return path.replace(/[,()]/g, '-');
+    if (percentTwenty.includes(version)) return path.replace(',', ',%20');
+    if (version === 'LATEST' && !path.endsWith('.html')) {
+        if (path.includes('#')) return path.replace('#', '.html#');
+        return path + '.html';
+    }
     return path;
 }
