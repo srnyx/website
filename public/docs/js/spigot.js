f92448bc31a77930d59b64441d21af3a83db8806
srnyx <25808801+srnyx@users.noreply.github.com>
2023-12-28 21:50:31 -0500

    Make Spigot Docs buttons match theme

--- public/docs/js/spigot.js
+++ public/docs/js/spigot.js
@@ -1,5 +1,35 @@
 const buttons = document.getElementsByClassName('versionButton');
 const path = window.location.href.split('/docs/spigot/')[1];
+const availableVersions = {
+    '1.19': ['1.19.3', '1.19.2', '1.19.1', '1.19'],
+    '1.18': ['1.18.2', '1.18.1', '1.18'],
+    '1.17': ['1.17.1', '1.17'],
+    '1.16': ['1.16.5', '1.16.4', '1.16.3', '1.16.2', '1.16.1'],
+    '1.15': ['1.15.2', '1.15.1', '1.15'],
+    '1.14': ['1.14.4', '1.14.3', '1.14.2', '1.14.1', '1.14'],
+    '1.13': ['1.13.2', '1.13.1', '1.13'],
+    '1.12': ['1.12.2', '1.12.1', '1.12'],
+    '1.11': ['1.11.2', '1.11.1', '1.11'],
+    '1.10': ['1.10.2', '1.10'],
+    '1.9': ['1.9.4', '1.9.2', '1.9'],
+    '1.8': ['1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8'],
+    '1.7': ['1.7.10', '1.7.9', '1.7.8', '1.7.5', '1.7.2'],
+    '1.6': ['1.6.2'],
+    '1.5': ['1.5.2'],
+};
+
+// Add buttons
+const buttonsElement = document.getElementsByClassName('buttons')[0];
+for (const version in availableVersions) {
+    buttonsElement.innerHTML += `<b>${version}:</b>`;
+    availableVersions[version].forEach(subVersion => {
+        const button = document.createElement('button');
+        button.classList.add('commonButton', 'versionButton');
+        button.textContent = subVersion;
+        buttonsElement.appendChild(button);
+    });
+    buttonsElement.innerHTML += '<br>';
+}
 
 // Set display path
 if (path) {
@@ -12,6 +42,7 @@ if (path) {
     document.getElementById('header').textContent = displayPath.replaceAll('/', '.');
 }
 
+// Add button listeners
 for (const button of buttons) {
     button.addEventListener('click', () => {
         const version = button.textContent;
@@ -24,7 +55,6 @@ for (const button of buttons) {
 
 const dashes = ['1.15.2', '1.15.1', '1.15', '1.14.4', '1.14.3', '1.14.2', '1.14.1', '1.14', '1.13.2', '1.13.1', '1.13', '1.12.2', '1.12.1', '1.12']
 const percentTwenty = ['1.11.2', '1.11.1', '1.11', '1.10.2', '1.10', '1.9.4', '1.9.2', '1.9.1', '1.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8', '1.7.10', '1.7.9', '1.7.8', '1.7.5', '1.7.2', '1.6.2', '1.5']
-
 function getPath(version) {
     if (!path) return;
     if (dashes.includes(version)) return path.replace(/[,()]/g, '-');
@@ -32,7 +62,6 @@ function getPath(version) {
     if (version === 'LATEST' && !path.endsWith('.html')) {
         if (!path.includes('#')) return path + '.html';
         if (!path.includes('.html#')) return path.replace('#', '.html#');
-        return path;
     }
     return path;
 }
