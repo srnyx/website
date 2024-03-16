34ea5f384d17bab28f09c12d9781daee512960dc
srnyx <25808801+srnyx@users.noreply.github.com>
2024-03-16 16:07:55 -0400

    Make Spigot Docs buttons anchors

--- public/docs/js/spigot.js
+++ public/docs/js/spigot.js
@@ -1,4 +1,3 @@
-const buttons = document.getElementsByClassName('versionButton');
 const path = window.location.href.split('/docs/spigot/')[1];
 const availableVersions = {
     '1.20': ['1.20.3', '1.20.2', '1.20.1', '1.20'],
@@ -18,18 +17,37 @@ const availableVersions = {
     '1.6': ['1.6.2'],
     '1.5': ['1.5.2'],
 };
+const dashes = ['1.15.2', '1.15.1', '1.15', '1.14.4', '1.14.3', '1.14.2', '1.14.1', '1.14', '1.13.2', '1.13.1', '1.13', '1.12.2', '1.12.1', '1.12']
+const percentTwenty = ['1.11.2', '1.11.1', '1.11', '1.10.2', '1.10', '1.9.4', '1.9.2', '1.9.1', '1.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8', '1.7.10', '1.7.9', '1.7.8', '1.7.5', '1.7.2', '1.6.2', '1.5']
 
 // Add buttons
 const buttonsElement = document.getElementsByClassName('buttons')[0];
 for (const version in availableVersions) {
     buttonsElement.innerHTML += `<b>${version}:</b>`;
     availableVersions[version].forEach(subVersion => {
-        const button = document.createElement('button');
+        const button = document.createElement('a');
         button.classList.add('commonButton', 'versionButton');
+        button.target = '_blank';
         button.textContent = subVersion;
+
+        // Set URL
+        let url = `https://helpch.at/docs/${subVersion}`;
+        const compiledPath = getPath(subVersion);
+        if (compiledPath) url += `/${compiledPath}`;
+        button.href = url
+
         buttonsElement.appendChild(button);
     });
     buttonsElement.innerHTML += '<br>';
+}
+
+// Set latest button URL
+const latestButton = document.getElementById('latest');
+if (latestButton) {
+    let url = 'https://hub.spigotmc.org/javadocs/spigot';
+    const compiledPath = getPath('LATEST');
+    if (compiledPath) url += `/${compiledPath}`;
+    latestButton.href = url;
 }
 
 // Set display path
@@ -43,19 +61,6 @@ if (path) {
     document.getElementById('header').textContent = displayPath.replaceAll('/', '.');
 }
 
-// Add button listeners
-for (const button of buttons) {
-    button.addEventListener('click', () => {
-        const version = button.textContent;
-        const compiledPath = getPath(version);
-        let url = version === 'LATEST' ? 'https://hub.spigotmc.org/javadocs/spigot' : `https://helpch.at/docs/${version}`;
-        if (compiledPath) url += `/${compiledPath}`;
-        window.location.href = url;
-    });
-}
-
-const dashes = ['1.15.2', '1.15.1', '1.15', '1.14.4', '1.14.3', '1.14.2', '1.14.1', '1.14', '1.13.2', '1.13.1', '1.13', '1.12.2', '1.12.1', '1.12']
-const percentTwenty = ['1.11.2', '1.11.1', '1.11', '1.10.2', '1.10', '1.9.4', '1.9.2', '1.9.1', '1.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8', '1.7.10', '1.7.9', '1.7.8', '1.7.5', '1.7.2', '1.6.2', '1.5']
 function getPath(version) {
     if (!path) return;
     if (dashes.includes(version)) return path.replace(/[,()]/g, '-');
