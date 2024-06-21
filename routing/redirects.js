4289a64c498f25a2ba691e0bdf3514da266294d4
srnyx <25808801+srnyx@users.noreply.github.com>
2024-06-20 22:31:33 -0400

    Add `/letterboxd`

--- routing/redirects.js
+++ routing/redirects.js
@@ -148,6 +148,7 @@ const object = {
         },
         dynamicReplace: {
             "letterbox": "https://letterboxd.com/srnyx",
+            "letterboxd": "https://letterboxd.com/srnyx"
         }
     },
     seg: {
